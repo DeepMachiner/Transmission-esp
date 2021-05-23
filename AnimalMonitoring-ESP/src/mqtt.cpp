62f75a2e64ec0dd2cf1f07ff4865e67b41107b39
Dhruv Puri <74000367+DeepMachiner@users.noreply.github.com>
2021-05-23 08:24:17 +0530

    Add files via upload

--- AnimalMonitoring-ESP/src/mqtt.cpp
+++ AnimalMonitoring-ESP/src/mqtt.cpp
@@ -1,23 +1,23 @@
 #include <PubSubClient.h>
-#include<WiFi.h>
+#include <WiFi.h>
 
 #include "mqtt.h"
 #include "credentials.h"
-
 
 extern int MPU_PKT_SIZE;
 WiFiClient wifiClient;
 PubSubClient mqttClient(wifiClient);
 
 boolean mqttInitCompleted = false;
+//setting mac address of the ESP as the client ID
 String clientId = "IoTPractice-" + String(WiFi.macAddress());
 
 /* Incoming data callback. */
-void dataCallback(char* topic, byte* payload, unsigned int length)
+void dataCallback(char *topic, byte *payload, unsigned int length)
 {
   char payloadStr[length + 1];
   memset(payloadStr, 0, length + 1);
-  strncpy(payloadStr, (char*)payload, length);
+  strncpy(payloadStr, (char *)payload, length);
   Serial.printf("Data    : dataCallback. Topic : [%s]\n", topic);
   Serial.printf("Data    : dataCallback. Payload : %s\n", payloadStr);
 }
@@ -28,7 +28,8 @@ void performConnect()
   while (!mqttClient.connected())
   {
     Serial.printf("Trace   : Attempting MQTT connection...\n");
-    if (mqttClient.connect(clientId.c_str(), MQTT_USERNAME, MQTT_KEY))
+    //connect to the client and last will messages, just in case WIFI fails
+    if (mqttClient.connect(clientId.c_str(), MQTT_USERNAME, MQTT_KEY,Acceleration,0, true, "Disconnected from the server"))
     {
       Serial.printf("Trace   : Connected to Broker.\n");
 
@@ -44,7 +45,7 @@ void performConnect()
   }
 }
 
-boolean MQTTPublish(const char* topic, char* payload)
+boolean MQTTPublish(const char *topic, char *payload)
 {
   boolean retval = false;
   if (mqttClient.connected())
@@ -54,7 +55,7 @@ boolean MQTTPublish(const char* topic, char* payload)
   return retval;
 }
 
-boolean MQTTSubscribe(const char* topicToSubscribe)
+boolean MQTTSubscribe(const char *topicToSubscribe)
 {
   boolean retval = false;
   if (mqttClient.connected())
@@ -70,8 +71,13 @@ boolean MQTTIsConnected()
 }
 
 void MQTTBegin()
-{ //mqttClient.setBufferSize(MPU_PKT_SIZE);
+
+{ //uncomment if you want to change the maximum packet size
+  //mqttClient.setBufferSize(MPU_PKT_SIZE);
+
+  //uncomment if you want to increase the Alive timme
   //mqttClient.setKeepAlive(KeepAlive);
+
   mqttClient.setServer(MQTT_HOST, MQTT_PORT);
   mqttClient.setCallback(dataCallback);
   mqttInitCompleted = true;
@@ -79,7 +85,7 @@ void MQTTBegin()
 
 void MQTTLoop()
 {
-  if(mqttInitCompleted)
+  if (mqttInitCompleted)
   {
     if (!MQTTIsConnected())
     {
